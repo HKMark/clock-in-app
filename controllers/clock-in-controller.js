7d71c26200f46755bf815b2e41361bf864ee2ebb
HKMark <HongKongMark@Gmail.com>
2023-04-25 01:53:15 +0800

    feat: add index page

---
+++ controllers/clock-in-controller.js
@@ -0,0 +1,8 @@
+const clockInController = {
+
+  getClockIns: (req, res) => {
+    return res.render('clock-ins')
+  }
+}
+
+module.exports = clockInController
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const router = express.Router()
+const clockInController = require('../controllers/clock-in-controller')
 
-router.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+router.get('/clock-ins', clockInController.getClockIns)
+
+router.use('/', (req, res) => res.redirect('/clock-ins'))
 
 module.exports = router
