95103be3694ec0741da118cc99a62a02822c2cf0
HKMark <HongKongMark@Gmail.com>
2023-04-25 02:34:58 +0800

    feat: add admin index page

---
+++ controllers/admin-controller.js
@@ -0,0 +1,6 @@
+const adminController = {
+  getClockIns: (req, res) => {
+    return res.render('admin/clock-ins')
+  }
+}
+module.exports = adminController
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const router = express.Router()
 const clockInController = require('../controllers/clock-in-controller')
+const admin = require('./modules/admin')
 
+router.use('/admin', admin)
 router.get('/clock-ins', clockInController.getClockIns)
-
 router.use('/', (req, res) => res.redirect('/clock-ins'))
 
 module.exports = router
