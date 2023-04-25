e1ed60322aeefdd43ec782a8c41f8c8d2df8c672
HKMark <HongKongMark@Gmail.com>
2023-04-25 21:35:38 +0800

    feat: passport init & signin

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,11 +1,20 @@
-// const bcrypt = require('bcryptjs')
-// const db = require('../models')
-// const { User } = db
+const bcrypt = require('bcryptjs')
+const db = require('../models')
+const { User } = db
 
-// const userController = {
-//   signInPage: (req, res) => {
-//     res.render('signin')
-//   }
-// }
+const userController = {
+  signInPage: (req, res) => {
+    res.render('signin')
+  },
+  signIn: (req, res) => {
+    req.flash('success_messages', '成功登入！')
+    res.redirect('/clock-ins')
+  },
+  logout: (req, res) => {
+    req.flash('success_messages', '登出成功！')
+    req.logout()
+    res.redirect('/signin')
+  }
+}
 
-// module.exports = userController
+module.exports = userController
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,17 @@
 const express = require('express')
 const router = express.Router()
-const clockInController = require('../controllers/clock-in-controller')
-const { generalErrorHandler } = require('../middleware/error-handler')
+const passport = require('../config/passport')
 const admin = require('./modules/admin')
+const clockInController = require('../controllers/clock-in-controller')
+const userController = require('../controllers/user-controller')
+const { generalErrorHandler } = require('../middleware/error-handler')
 
 router.use('/admin', admin)
+
+router.get('/signin', userController.signInPage)
+router.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn)
+router.get('/logout', userController.logout)
+
 router.get('/clock-ins', clockInController.getClockIns)
 router.use('/', (req, res) => res.redirect('/clock-ins'))
 router.use('/', generalErrorHandler)
