b20527046cc41fe1c7ae3e899ed0b7d505d88e53
HKMark <HongKongMark@Gmail.com>
2023-04-25 16:14:39 +0800

    feat: add user model

---
+++ migrations/20230425080620-create-user.js
@@ -0,0 +1,49 @@
+'use strict'
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      email: {
+        type: Sequelize.STRING
+      },
+      password: {
+        type: Sequelize.STRING
+      },
+      account_role: {
+        type: Sequelize.STRING
+      },
+      employee_id: {
+        type: Sequelize.INTEGER
+      },
+      job_title: {
+        type: Sequelize.STRING
+      },
+      department: {
+        type: Sequelize.STRING
+      },
+      work_address: {
+        type: Sequelize.STRING
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    })
+  },
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable('Users')
+  }
+}
