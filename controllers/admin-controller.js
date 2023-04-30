61b61328811bd7a9f4c771b2a5b24243f9eb748e
HKMark <HongKongMark@Gmail.com>
2023-04-30 21:15:49 +0800

    modify account create function for admin

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -27,7 +27,7 @@ const adminController = {
       }))
       .then(() => {
         req.flash('success_messages', '成功建立帳號！')
-        res.redirect('/admin')
+        res.redirect('/clock-ins')
       })
       .catch(err => next(err))
   }
--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -4,8 +4,8 @@ const adminController = require('../../controllers/admin-controller')
 const { authenticatedAdmin } = require('../../middleware/auth')
 const { generalErrorHandler } = require('../../middleware/error-handler')
 
-router.get('/signup', adminController.signUpPage)
-router.post('/signup', adminController.signUp)
+router.get('/signup', authenticatedAdmin, adminController.signUpPage)
+router.post('/signup', authenticatedAdmin, adminController.signUp)
 
 router.get('/attendances', authenticatedAdmin, adminController.getAttendances)
 router.get('/', (req, res) => res.redirect('/admin/attendances'))
